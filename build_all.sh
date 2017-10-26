1eb20b6ab93cb95f12fc81e2092f0a75c5835c5e
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-10-26 12:46:20 +0200

    Add a script to build all images.

---
+++ build_all.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+set -e
+
+./build_mbs-ubuntu-1404-x64.sh
+./build_mbs-ubuntu-1404-x86.sh
+./build_mbs-ubuntu-1604-x64.sh
+./build_mbs-ubuntu-1604-x86.sh
+./build_mbs-ubuntu-1704-x64.sh
+./build_mbs-ubuntu-1704-x86.sh
+./build_mbs-ubuntu-1710-x64.sh
+./build_mbs-ubuntu-1710-x86.sh
